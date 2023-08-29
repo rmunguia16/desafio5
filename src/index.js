9e8e8f80a0eca74fa9b3ef6952efe55807dd0c31
rmunguia16 <rmunguia16@comunidad.unam.mx>
2023-08-29 15:36:37 -0600

    2

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import cartRouter from "./routes/cart.routes.js";
 import { engine } from 'express-handlebars';
 import __dirname from "./utils.js";
 import path from 'path';
+import {Server} from 'socket.io';
 
 const app = express();
+const httpServer = app.listen(8080, () => {console.log('\nServidor escuchando en http://localhost:8080');});
+const socketServer = new Server(httpServer);
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(`${__dirname}/public`));
@@ -25,6 +29,3 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(8080, () => {
-    console.log('\nServidor escuchando en http://localhost:8080');
-});
